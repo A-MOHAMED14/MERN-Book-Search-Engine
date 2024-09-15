35ef9738b9d4d7fdcf6fe122e570bd5074fee4ca
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-09-15 11:10:01 +0100

    Implement addUser resolver to handle user creation and token generation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,19 @@
 const { User } = require("../models/index.js");
+const { signToken, AuthenticationError } = require("../utils/auth.js");
 
 const resolvers = {
   Query: {
     me: async (parent, { userId, username }) => {
       return User.findOne({ $or: [{ _id: userId }, { username: username }] });
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+
+      return { token, user };
     },
   },
 };
