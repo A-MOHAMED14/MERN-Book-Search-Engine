b084c0897428c8437bd9f1cad6d57f1dd3f21ad4
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-09-15 11:00:56 +0100

    Import the User model and Implement me Query resolver to find user by userId or username

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,1 +1,9 @@
-const {User} = require('../server')
+const { User } = require("../models/index.js");
+
+const resolvers = {
+  Query: {
+    me: async (parent, { userId, username }) => {
+      return User.findOne({ $or: [{ _id: userId }, { username: username }] });
+    },
+  },
+};
