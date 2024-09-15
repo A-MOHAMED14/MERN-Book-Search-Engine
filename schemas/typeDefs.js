7134ed2d8e2e10a24752c13feddfc901be728ba7
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-09-15 10:35:20 +0100

    Add BookInput input type and Add Mutation types for login, addUser, saveBook, and removeBook

--- schemas/typeDefs.js
+++ schemas/typeDefs.js
@@ -22,6 +22,22 @@ type Auth {
 }
 
 type Query {
-me(_id: ID!, username: string): User
+me(_id: ID, username: String): User
+}
+
+input BookInput {
+ bookId: String!
+ authors: [String]!
+ description: String!
+ title: String!
+ image: String!
+ link: String!
+}
+
+type Mutation {
+ login(username: String, email: String, password: String!): Auth
+ addUser(username: String!, email: String!, password: String!): Auth
+ saveBook(_id: ID!, book: BookInput!): User
+ removeBook(_id: ID!): User
 }
 `;
